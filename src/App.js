93b2622ea53ea65b2344e69f1253c100d90653ee
WilfredoAcosta <wilfredo.acosta10@gmail.com>
2023-09-01 16:21:56 -0600

    Form hecho, faltan checkbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { PlusOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import {
+  Button,
+  Cascader,
+  Checkbox,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Slider,
+  Switch,
+  TreeSelect,
+  Upload,
+} from 'antd';
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+const normFile = (e) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
+const FormDisabledDemo = () => {
+  const [componentDisabled, setComponentDisabled] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+  
+      <Form
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 14,
+        }}
+        layout="horizontal"
+        disabled={componentDisabled}
+        style={{
+          maxWidth: 600,
+        }}
+      >
+       
+        <Form.Item label="Nombre">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Tamaño">
+          <Select>
+            <Select.Option value="demo">Pizza personal</Select.Option>
+            <Select.Option value="demo">Pizza mediana</Select.Option>
+            <Select.Option value="demo">Pizza grande</Select.Option>
+          </Select>
+        </Form.Item>
 
-export default App;
+        
+        <Form.Item label="Button">
+          <Button>Button</Button>
+        </Form.Item>
+
+      </Form>
+    </>
+  );
+};
+export default () => <FormDisabledDemo />;
